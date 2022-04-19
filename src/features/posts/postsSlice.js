0e8c1b31baa4eb796e4ee9d0adca5c048369ef99
Springhawk27 <sajjad0mahmud@gmail.com>
2022-04-19 11:45:55 +0600

    AddPostForm component with redux implemented

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -17,10 +17,14 @@ const postSlice = createSlice({
     name: 'posts',
     initialState,
     reducers: {
-
-    },
+        postAdded(state, action) {
+            state.push(action.payload)
+        },
+    }
 });
 
 export const selectAllPosts = (state) => state.posts;
+
+export const { postAdded } = postSlice.actions;
 
 export default postSlice.reducer;
