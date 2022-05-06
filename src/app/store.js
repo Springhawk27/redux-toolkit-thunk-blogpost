5d5ad0e311d8d1de95b1a69e763e939cb7d7e502
Springhawk27 <sajjad0mahmud@gmail.com>
2022-05-06 23:27:39 +0600

    user slice added

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import postReducers from '../features/posts/postsSlice';
+import postsReducers from '../features/posts/postsSlice';
+import usersReducers from '../features/users/usersSlice'
 
 export const store = configureStore({
     reducer: {
-        posts: postReducers,
+        posts: postsReducers,
+        users: usersReducers,
     }
 })
