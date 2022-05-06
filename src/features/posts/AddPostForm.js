357404e6ce55451e43618286d2f45bbea466b2ab
Springhawk27 <sajjad0mahmud@gmail.com>
2022-05-07 00:09:25 +0600

    author option added

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 // import { nanoid } from '@reduxjs/toolkit';
 
 import { postAdded } from './postsSlice';
+// new 1
+import { selectAllUsers } from '../users/usersSlice';
 
 
 const AddPostForm = () => {
@@ -10,9 +12,16 @@ const AddPostForm = () => {
 
     const [title, setTitle] = useState('')
     const [content, setContent] = useState('')
+    // new 1
+    const [userId, setUserId] = useState('')
+
+    // new 1
+    const users = useSelector(selectAllUsers);
 
     const onTitleChanged = e => setTitle(e.target.value)
     const onContentChanged = e => setContent(e.target.value)
+    // new 1
+    const onAuthorChanged = e => setUserId(e.target.value)
 
     const onSavePostClicked = () => {
         if (title && content) {
@@ -22,12 +31,23 @@ const AddPostForm = () => {
                 //     title,
                 //     content
                 // })
-                postAdded(title, content)
+                postAdded(title, content, userId)
             )
             setTitle('')
             setContent('')
         }
-    }
+    };
+
+    // new 1
+    const canSave = Boolean(title) && Boolean(content) && Boolean(userId)
+
+
+    // new 1
+    const usersOptions = users.map(user => (
+        <option key={user.id} value={user.id}>
+            {user.name}
+        </option>
+    ))
 
     return (
         <section>
@@ -41,6 +61,11 @@ const AddPostForm = () => {
                     value={title}
                     onChange={onTitleChanged}
                 />
+                <label htmlFor="postAuthor">Author:</label>
+                <select id="postAuthor" value={userId} onChange={onAuthorChanged}>
+                    <option value=""></option>
+                    {usersOptions}
+                </select>
 
                 <label htmlFor="postContent">Content:</label>
                 <textarea
@@ -52,6 +77,7 @@ const AddPostForm = () => {
                 <button
                     type="button"
                     onClick={onSavePostClicked}
+                    disabled={!canSave}
                 >Save Post</button>
             </form>
         </section>
--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -30,12 +30,13 @@ const postSlice = createSlice({
             reducer(state, action) {
                 state.push(action.payload)
             },
-            prepare(title, content) {
+            prepare(title, content, userId) {
                 return {
                     payload: {
                         id: nanoid(),
                         title,
                         content,
+                        userId,
                     }
                 }
             }
